8c6c481bb538859911cd853307a0470eeadf9c50
Haaris01 <mdharis00000@gmail.com>
2022-03-07 09:13:00 +0530

    Updates

---
+++ precache-manifest.fbbbd4e404cebb47f57d7c386a3d1826.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cceb1b244dc301b1c871",
+    "url": "/lightsout/static/css/main.e1ec6435.chunk.css"
+  },
+  {
+    "revision": "cceb1b244dc301b1c871",
+    "url": "/lightsout/static/js/main.cceb1b24.chunk.js"
+  },
+  {
+    "revision": "b43a7eab4c27c21e54e5",
+    "url": "/lightsout/static/js/1.b43a7eab.chunk.js"
+  },
+  {
+    "revision": "5140b50a75f4729ab6d5",
+    "url": "/lightsout/static/js/runtime~main.5140b50a.js"
+  },
+  {
+    "revision": "7ad099a6a483782a272af3e084f2b6b3",
+    "url": "/lightsout/index.html"
+  }
+];
